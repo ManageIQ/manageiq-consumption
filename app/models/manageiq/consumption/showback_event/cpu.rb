e9649126551251cb2d9b3972a4c213a2489a27f1
lpichler <lpichler@redhat.com>
2017-08-10 12:40:41 +0200

    Add allocated number cpus for VM

--- app/models/manageiq/consumption/showback_event/cpu.rb
+++ app/models/manageiq/consumption/showback_event/cpu.rb
@@ -17,15 +17,20 @@ module ManageIQ::Consumption::ShowbackEvent::CPU
     return value
   end
 
+  def CPU_allocated_number_cpus(_value)
+    @metrics.average(:derived_vm_numvcpus)
+  end
+
   #
   #  Return the max number of cpu for object
   #
   def CPU_max_number_of_cpu(value)
     if resource.class.name.ends_with?("Container")
-      numcpus = resource.vim_performance_states.last.state_data[:numvcpus]
+      numcpus = resource.vim_performance_states.last.try(:state_data) ? resource.vim_performance_states.last.state_data[:numvcpus] : 0
     else
       numcpus = if resource.methods.include?(:cpu_total_cores) then resource.cpu_total_cores else 0 end
     end
-    [value, numcpus].max.to_i
+
+    [value, numcpus].compact.max.to_i
   end
 end
