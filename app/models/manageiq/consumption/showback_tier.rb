1c3bcf362a9fcf194954735c0c88ab09936a458e
Alberto Jesus Gutierrez Juanes <alberto@iMac-de-Alberto.local>
2017-09-21 09:54:44 +0200

    tiers methods

--- app/models/manageiq/consumption/showback_tier.rb
+++ app/models/manageiq/consumption/showback_tier.rb
@@ -23,10 +23,67 @@ module ManageIQ::Consumption
     validates :tier_start_value,  :numericality => {:greater_than_or_equal_to => 0, :less_than => Float::INFINITY}
     validates :tier_end_value,    :numericality => {:greater_than_or_equal_to => 0}
 
+    before_save :validate_interval
+
     def name
       "#{showback_rate.category}:#{showback_rate.measure}:#{showback_rate.dimension}:Tier:#{tier_start_value}-#{tier_end_value}"
     end
 
+    def validate_interval
+      ManageIQ::Consumption::ShowbackTier.where(:showback_rate => showback_rate).each do |tier|
+        next if tier == self
+        #returns true == overlap, false == no overlap
+        if tier.tier_end_value == Float::INFINITY and (tier_start_value>tier.tier_start_value or tier_end_value>tier.tier_start_value)
+          raise _("Interval or subinterval is in a tier with Infinity at the end")
+        end
+        if !(range.to_set & tier.range.to_set).empty?
+          raise _("Interval or subinterval is in another tier")
+        end
+      end
+    end
 
+    def range
+      (tier_start_value..tier_end_value) # or as an array, or however you want to return it
+    end
+
+    def set_range(srange, erange)
+      self.tier_start_value = srange
+      self.tier_end_value   = erange
+      self.save
+    end
+
+    def divide_tier(value)
+      old = tier_end_value
+      self.tier_end_value = value
+      self.save
+      new_tier = self.clone
+      new_tier.tier_end_value = old
+      new_tier.tier_start_value = value
+      new_tier.save
+    end
+
+    def self.to_float(s)
+      if s.to_s.include?("Infinity")
+        Float::INFINITY
+      else
+        s
+      end
+    end
+
+    def includes?(value)
+      starts_with_zero? && value.zero? || value > tier_start_value && value.to_f <= tier_end_value
+    end
+
+    def starts_with_zero?
+      tier_start_value.zero?
+    end
+
+    def ends_with_infinity?
+      tier_end_value == Float::INFINITY
+    end
+
+    def free?
+      fixed_rate.zero? && variable_rate.zero?
+    end
   end
 end
--- spec/models/showback_tier_spec.rb
+++ spec/models/showback_tier_spec.rb
@@ -23,16 +23,16 @@ RSpec.describe ManageIQ::Consumption::ShowbackTier, :type => :model do
 
     it 'has a Money fixed_rate' do
       expect(described_class).to monetize(:fixed_rate)
-      ch = FactoryGirl.create(:showback_tier, :fixed_rate => Money.new(256_000_000, 'US8'))
-      expect(ch).to be_valid
-      expect(ch.fixed_rate.exchange_to('USD').format).to eq('$2.56')
+      showback_tier.fixed_rate = Money.new(256_000_000, 'US8'
+      expect(showback_tier).to be_valid
+      expect(showback_tier.fixed_rate.format).to eq('$2.56')
     end
 
     it 'has a Money variable_rate' do
       expect(described_class).to monetize(:variable_rate)
-      ch = FactoryGirl.create(:showback_tier, :variable_rate => Money.new(675_000_000, 'US8'))
-      expect(ch).to be_valid
-      expect(ch.variable_rate.exchange_to('USD').format).to eq('$6.75')
+      showback_tier.variable_rate = Money.new(675_000_000, 'US8')
+      expect(showback_tier).to be_valid
+      expect(showback_tier.variable_rate.format).to eq('$6.75')
     end
 
     it '#fixed_rate_per_time included in VALID_INTERVAL_UNITS is valid' do
@@ -79,6 +79,91 @@ RSpec.describe ManageIQ::Consumption::ShowbackTier, :type => :model do
       showback_tier.variable_rate_per_unit = nil
       showback_tier.valid?
       expect(showback_tier.errors.details[:variable_rate_per_unit]).to include(:error => :exclusion, :value => nil)
+    end
+
+    context 'validate intervals' do
+      let(:showback_rate){FactoryGirl.create(:showback_rate)}
+      let(:showback_t1){FactoryGirl.create(:showback_tier, :showback_rate => showback_rate)}
+      let(:showback_t2){FactoryGirl.build(:showback_tier, :showback_rate => showback_rate)}
+
+      it '#there is a showbackTier just defined with Float::INFINITY you can add another in this interval' do
+        showback_t2.tier_start_value = 5
+        showback_t2.tier_end_value = 10
+        expect { showback_t2.save }.to raise_error(RuntimeError, _("Interval or subinterval is in a tier with Infinity at the end"))
+      end
+
+=begin
+      it '#there is a showbackTier just defined in this interval' do
+        showback_t1.tier_start_value = 2
+        showback_t1.tier_end_value = 7
+        puts "1"
+        showback_t1.save
+        puts "2"
+        showback_t2.tier_start_value = 5
+        showback_t2.tier_end_value = 10
+        puts showback_t1.inspect
+        expect { showback_t2.save }.to raise_error(RuntimeError, _("Interval or subinterval is in another tier"))
+      end
+=end
+    end
+
+  end
+
+  describe 'tier methods' do
+    context 'interval methods' do
+      let(:showback_tier){FactoryGirl.build(:showback_tier)}
+      it '#range return the range of tier_start_value and tier_end_value' do
+        expect(showback_tier.range).to eq(0..Float::INFINITY)
+      end
+
+      it '#includes? method' do
+        showback_tier.tier_start_value = 2
+        showback_tier.tier_end_value = 4
+        expect(showback_tier.includes?(3)).to be_truthy
+        expect(showback_tier.includes?(5)).to be_falsey
+        showback_tier.tier_end_value = Float::INFINITY
+        expect(showback_tier.includes?(500)).to be_truthy
+        expect(showback_tier.includes?(1)).to be_falsey
+      end
+
+      it '#starts_with_zero? method' do
+        showback_tier.tier_start_value = 2
+        expect(showback_tier.starts_with_zero?).to be_falsey
+        showback_tier.tier_start_value = 0
+        expect(showback_tier.starts_with_zero?).to be_truthy
+      end
+
+      it '#ends_with_infinity?method' do
+        expect(showback_tier.ends_with_infinity?).to be_truthy
+        showback_tier.tier_end_value = 10
+        expect(showback_tier.ends_with_infinity?).to be_falsey
+      end
+
+      it '#free? method' do
+        expect(showback_tier.free?).to be_falsey
+        showback_tier.fixed_rate = 0
+        expect(showback_tier.free?).to be_falsey
+        showback_tier.fixed_rate = 10
+        showback_tier.variable_rate = 0
+        expect(showback_tier.free?).to be_falsey
+        showback_tier.fixed_rate = 0
+        expect(showback_tier.free?).to be_truthy
+      end
+
+      it 'to_float method' do
+        showback_tier.tier_start_value = 2
+        showback_tier.tier_end_value = Float::INFINITY
+        expect(described_class.to_float(showback_tier.tier_start_value)).to eq(2)
+        expect(described_class.to_float(showback_tier.tier_end_value)).to eq(Float::INFINITY)
+      end
+
+=begin
+      it 'set_range method' do
+        sb = FactoryGirl.build(:showback_tier)
+        sb.set_range(5,7)
+        expect(sb.range).to eq(5..7)
+      end
+=end
     end
   end
 
