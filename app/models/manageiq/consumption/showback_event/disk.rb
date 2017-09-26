2ede4f43f5ef2e413c238d44e56e9961bd8dbb49
lpichler <lpichler@redhat.com>
2017-09-26 15:17:53 +0200

    Add missing methods for calculating metric values
    added only for fixing spec, functionality is not provided
    for gathering values from metric rollups

---
+++ app/models/manageiq/consumption/showback_event/disk.rb
@@ -0,0 +1,5 @@
+module ManageIQ::Consumption::ShowbackEvent::DISK
+  # for old chargeback integration
+  def disk_io_disk_usage_rate_average
+  end
+end
---
+++ app/models/manageiq/consumption/showback_event/net.rb
@@ -0,0 +1,5 @@
+module ManageIQ::Consumption::ShowbackEvent::NET
+  # for old chargeback integration
+  def net_io_net_usage_rate_average
+  end
+end
